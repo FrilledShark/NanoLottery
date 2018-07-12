ac0066e56e6513d63308a74aae014cf7ab27881c
FrilledShark <frilledshark@tuta.io>
2018-07-12 19:20:32 +0300

    Fully working lottery. Few features missing, but working PoC.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from modules.database import Ticket, Lottery, tables, db
+from decimal import Decimal
+from modules.fair import last_blockchain
 
 import os, json  # Reading config file
 dirname = os.path.dirname(__file__)
@@ -14,7 +16,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    for lottery in Lottery.select().order_by(Lottery.endblock.desc()):
+        lottery = lottery
+        break
+
+    # Calculating pot
+    pot = Decimal(0)
+    for ticket in lottery.tickets:
+        pot += Decimal("0.01")
+
+    # Calculating remaining time
+
+    remaining_blocks = lottery.endblock - last_blockchain()
+
+    remaining_time = remaining_blocks*10
+    table = []
+    for ticket in lottery.tickets.order_by(Ticket.time.desc()):
+        ticket_info = {"date": ticket.time, "ticket": ticket.ticket, "account": ticket.account}
+        table.append(ticket_info)
+
+    return render_template("index.html", pot=pot, time=remaining_time, account=config["account"], table=table)
 
 
 if __name__ == "__main__":
