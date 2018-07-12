03a35d4a619fee8d5a6631ba2a67d9729bc6ca2f
FrilledShark <frilledshark@tuta.io>
2018-07-12 22:37:13 +0300

    More stats on /ticket and /lottery

--- app.py
+++ app.py
@@ -32,23 +32,38 @@ def index():
     remaining_time = remaining_blocks*10
     table = []
     for ticket in lottery.tickets.order_by(Ticket.time.desc()):
-        ticket_info = {"date": ticket.time, "ticket": ticket.ticket, "account": ticket.account,
+        ticket_info = {"date": str(ticket.time)[:19], "ticket": ticket.ticket, "account": ticket.account,
                        "endblock": ticket.lottery.endblock}
         table.append(ticket_info)
 
     return render_template("index.html", pot=pot, time=remaining_time, account=config["account"], table=table, endblock=lottery.endblock)
 
 
-@app.route('/history', methods=['GET', 'POST'])
-def history():
-
+@app.route('/lottery', methods=['GET', 'POST'])
+def lottery():
     # Lotteries
+    lottery_table = []
+    for lottery in Lottery.select().order_by(Lottery.endblock.desc()):
+        pot = Decimal(0)
+        for ticket in lottery.tickets:
+            pot += Decimal("0.01")
+        lottery_dir = {"endblock": lottery.endblock, "time": str(lottery.time)[:19],
+                       "pot": pot, "roll": lottery.roll, "winner": lottery.winner}
+        lottery_table.append(lottery_dir)
+
+    return render_template("lottery.html", lottery_table=lottery_table)
 
 
+@app.route('/ticket', methods=['GET', 'POST'])
+def ticket():
     # Tickets
+    ticket_table = []
+    for ticket in Ticket.select().join(Lottery).order_by(Lottery.endblock.desc(), Ticket.ticket.desc()):
+        ticket_dir = {"endblock": ticket.lottery.endblock, "ticket": ticket.ticket,
+                      "time": str(ticket.time)[:19], "account": ticket.account, "hash": ticket.hash}
+        ticket_table.append(ticket_dir)
 
-    return render_template("history.html")
-
+    return render_template("ticket.html", ticket_table=ticket_table)
 
 
 if __name__ == "__main__":
